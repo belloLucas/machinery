d9a42fad52d5788a522508da99865afa4f6be7a5
belloLucas <lucasbellodev@gmail.com>
2025-07-23 17:08:57 -0300

    feat(api): initialize NestJS application with basic structure and configuration
    - Added package.json with dependencies and scripts for building, testing, and linting.
    - Created AppController and AppService with a simple "Hello World!" endpoint.
    - Implemented AppModule to configure global settings, including TypeORM and environment variables.
    - Set up main.ts for application bootstrap and Swagger documentation.
    - Added unit tests for AppController and end-to-end tests for the API.
    - Configured Jest for testing with a dedicated e2e configuration.
    - Created TypeScript configuration files for building and development.

---
+++ api/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MachineModule } from './machine/machine.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+    }),
+    MachineModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ api/src/main.ts
@@ -0,0 +1,34 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Machine API')
+    .setDescription('Gerenciamento de máquinas')
+    .setVersion('1.0')
+    .addTag('machines')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  console.log(`🚀 Application running on: http://localhost:${port}`);
+  console.log(`📚 API Documentation: http://localhost:${port}/api`);
+}
+bootstrap();
